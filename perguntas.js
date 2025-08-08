043317a12c418077da8f8187fcff8d34d5ce5ccd
giovanegorski <giovane.gorski.silva@escola.pr.gov.br>
2025-08-08 12:09:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'ser vivo', 
+    'Qual ser vivo que vive mais tempo?',
+    'esponja-do-mar'  
+)
+ 
+ 
+ criaCartao(
+    'cavalo', 
+    'Qual o maior cavalo do mundo?', 
+    'Shire chamado Sampson também conhecido como Mammoth' 
+ )
+ 
+ 
+ criaCartao(
+    'Prédio',
+    'qual é o maior prédio do mundo?', 
+    'O maior prédio do mundo hoje é o Burj Khalifa, localizado em Dubai, nos Emirados Árabes Unidos' 
+ )
+ 
+ 
+ criaCartao(
+    'Macaco', 
+    'Qual o macaco mais agil do mundo?', 
+    'O macaco mais ágil do mundo não tem uma resposta única, pois a agilidade varia entre as espécies e até mesmo entre indivíduos.No entanto, algumas espécies são conhecidas por sua grande habilidade de movimento e escalada, como os gibões, os símio-da-nova-guiné e os aranhas-micos. ' 
+ )
